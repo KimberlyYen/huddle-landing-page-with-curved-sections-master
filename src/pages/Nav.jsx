930fa28fbaa60cb95abdde1c2593ff25ab3ee994
Kimberly <work223305@gmail.com>
2023-04-17 22:46:19 +0800

    fix logo

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -1,14 +1,16 @@
-import logo from "../assets/logo.svg";
+// import logo from "../assets/logo.svg";
+import { ReactComponent as Logo } from "../assets/logo.svg";
+
 import Button from "../components/Button";
 
 function Nav() {
   return (
-    <div className="grid grid-cols-2">
-      <div>
-        <img src={logo} alt="logo" className="w-full h-full" />
+    <div className="flex">
+      <div className="w-24 text-[#FF51BF]">
+        <Logo />
       </div>
 
-      <div className="">
+      <div className="ml-auto">
         <Button />
       </div>
     </div>
